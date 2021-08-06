22606b8c4bd0366c6202c2f3d6b81efd1387857c
ermias111 <ermimichael@gmail.com>
2021-08-05 20:29:37 -0500

    node modules

---
+++ node/sum.js
@@ -0,0 +1,17 @@
+let sum = 0;
+function getNumber(){
+
+    readline.question("Insert a number (type stop to quit) ", num => {
+        if(num == "stop"){
+            console.log("Sum = " + sum);
+            readline.close();
+            return null;
+        }
+
+        sum += parseInt(num);
+
+        return getNumber();
+    })
+}
+
+getNumber();
